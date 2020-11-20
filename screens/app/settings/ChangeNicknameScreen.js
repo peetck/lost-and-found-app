0cff30eaf7faeb2ef14a8fab0a6f397fd32d216f
peetck <peekung602@gmail.com>
2020-11-21 00:20:17 +0700

    Update ChangeNicknameScreen.js

--- screens/app/settings/ChangeNicknameScreen.js
+++ screens/app/settings/ChangeNicknameScreen.js
@@ -2,49 +2,37 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import Constants from "expo-constants";
 import { CardStyleInterpolators } from "@react-navigation/stack";
-
 import { useSelector, useDispatch } from "react-redux";
-import { changeNickname } from "../../../store/actions/user";
 import i18n from "i18n-js";
+
+import { changeNickname } from "../../../store/actions/user";
+import MyTextInput from "../../../components/UI/MyTextInput";
 
 const ChangeNicknameScreen = (props) => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
-
-  {
-    /* state */
-  }
   const [text, setText] = useState(user.nickname);
 
-  {
-    /* function */
-  }
   const renderLengthOfText = () => {
     return <Text style={styles.content}>{text.length}/100</Text>;
   };
 
-  {
-    /* return */
-  }
+  const changeNicknameHandler = async () => {
+    await dispatch(changeNickname(text));
+    props.navigation.navigate("ProfileSetting");
+  };
+
   return (
     <View style={styles.screen}>
       {renderLengthOfText()}
-      <TextInput
-        style={styles.textInput}
+      <MyTextInput
         onChangeText={(text) => {
           setText(text);
         }}
         value={text}
       />
       <View style={{ marginTop: 12 }}>
-        <Button
-          title="Save"
-          color="#1b6ca8"
-          onPress={async () => {
-            await dispatch(changeNickname(text));
-            props.navigation.navigate("ProfileSetting");
-          }}
-        ></Button>
+        <Button title="Save" color="#1b6ca8" onPress={changeNicknameHandler} />
       </View>
     </View>
   );
@@ -59,13 +47,6 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     flex: 1,
-  },
-  textInput: {
-    borderWidth: 0.5,
-    borderRadius: 3,
-    height: 38,
-    fontSize: 18,
-    paddingHorizontal: 7,
   },
   content: {
     fontSize: 12,
